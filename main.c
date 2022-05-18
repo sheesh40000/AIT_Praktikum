797efee062351edcb492d6ff2255b72ebb51da67
sheesh40000 <105350640+sheesh40000@users.noreply.github.com>
2022-05-18 10:39:56 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-´#include <stdio.h>
+/*
+#include <stdio.h>
 #include "msg.h"
 
 #include "net/gcoap.h"
@@ -13,19 +14,22 @@ static const shell_command_t shell_commands[] = {
     { "test", "this is just a test.", gcoap_cli_cmd },
     { NULL, NULL, NULL }
 };
+*/
 
 int main(void)
 {
-    /* for the thread running the shell */
+/*     for the thread running the shell 
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
     server_init();
     puts("This is just a test from main#22");
-
+*/
     /* start shell */
     puts("Test up, running the shell now. main#25");
+/*
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
-
+*/
     /* should never be reached */
     return 0;
+    
 }
