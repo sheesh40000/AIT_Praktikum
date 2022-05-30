1c8f8e773952c5f13037788d6bcabaf5cda4f84f
sheesh40000 <105350640+sheesh40000@users.noreply.github.com>
2022-05-30 11:48:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+#include "msg.h"
 
+#include "net/gcoap.h"
 #include "shell.h"
+
+#include "gcoap_example.h"
+
+#define MAIN_QUEUE_SIZE (4)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
+static const shell_command_t shell_commands[] = {
+    { "coap", "CoAP example", gcoap_cli_cmd },
+    { NULL, NULL, NULL }
+};
 
 int main(void)
 {
     puts("Welcome to TESTAPP!\n");
     puts("Type `help` for help, type `saul` to see all SAUL devices\n");
 
+    server_init();
+    
     char line_buf[SHELL_DEFAULT_BUFSIZE];
-    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+    shell_run(shell_command_t, line_buf, SHELL_DEFAULT_BUFSIZE);
 
     return 0;
 }
