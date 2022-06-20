cf45d9c9506576d91cdd1464f5a0e9c022deef9d
sheesh40000 <105350640+sheesh40000@users.noreply.github.com>
2022-06-20 17:03:40 +0200

    Update coap_test.py

--- coap_test.py
+++ coap_test.py
@@ -16,7 +16,7 @@ async def get_addr():
     print(addr_mc)
     
     #################################
-    addr_mc = 'coap://[2001:67c:254:b0b2:affe:49a0:3f3d:8de6]'
+    #addr_mc = 'coap://[2001:67c:254:b0b2:affe:49a0:3f3d:8de6]'
     response = await protocol.request(Message(code=GET, uri=addr_mc + ".well-known/core")).response
     print("response19= {}". format(response.payload))
 
