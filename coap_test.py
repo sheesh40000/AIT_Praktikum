6866eacb24ea33016b41a09136ef97122b61077c
sheesh40000 <105350640+sheesh40000@users.noreply.github.com>
2022-06-20 18:05:39 +0200

    Update coap_test.py

--- coap_test.py
+++ coap_test.py
@@ -44,9 +44,9 @@ async def main():
     addr_mc = await get_addr(protocol)
     sensor_array = await get_sensors(protocol, addr_mc)
     await read_sensors(protocol, addr_mc, sensor_array)
-    protocol.shutdown()
+    await protocol.shutdown()
     
-    return NULL
+    return 1
 
     
 if __name__ == '__main__':
